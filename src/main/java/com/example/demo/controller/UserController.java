3e1002b07f8354372a40e3c982429bc200922f95
fan <610398596@qq.com>
2019-08-09 14:41:10 +0800

    Initial commit

---
+++ src/main/java/com/example/demo/controller/UserController.java
@@ -0,0 +1,41 @@
+package com.example.demo.controller;
+
+import com.example.demo.dao.UserMapper;
+import com.example.demo.entity.User;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("")
+public class UserController {
+    @Autowired
+    private UserMapper userMapper;
+
+    @GetMapping("/user")
+    public List<User> findAll() {
+        System.out.println("testtest");
+        return userMapper.findAll();
+    }
+
+    @GetMapping("/user/{userId}")
+    public User findById(@PathVariable Integer userId) {
+        return userMapper.selectByPrimaryKey(userId);
+    }
+
+    @PostMapping("/user")
+    public void insertUser(@RequestBody User user) {
+        userMapper.insert(user);
+    }
+
+    @PutMapping("/user/{userId}")
+    public void updateUser(@RequestBody User user,@PathVariable int userId) {
+        userMapper.updateByPrimaryKey(user,userId);
+    }
+
+    @DeleteMapping("/user/{userId}")
+    public void deleteById(@PathVariable Integer userId) {
+        userMapper.deleteByPrimaryKey(userId);
+    }
+}
---
+++ src/main/java/com/example/demo/dao/UserMapper.java
@@ -0,0 +1,26 @@
+package com.example.demo.dao;
+
+import com.example.demo.entity.User;
+import org.apache.ibatis.annotations.Param;
+
+
+import java.util.List;
+
+public interface UserMapper {
+
+    List<User> findAll();
+
+    int deleteByPrimaryKey(Integer id);
+
+    int insert(User record);
+
+    int insertSelective(User record);
+
+    User selectByPrimaryKey(Integer id);
+
+    int updateByPrimaryKeySelective(User record);
+
+    int updateByPrimaryKey(@Param("user") User user,@Param("userId") int userId);
+
+
+}
